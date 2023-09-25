9ae5b8d2e63ee3aedadc925fb7195aa9330cd2f9
moong2 <www.castlehi@gmail.com>
2023-09-25 18:35:10 +0900

    feat: basic type

---
+++ src/practice.ts
@@ -0,0 +1,19 @@
+let count = 0;
+count += 1;
+count = "갑자기 분위기 문자열";
+
+const message: string = "Hello World";
+
+const done: boolean = true;
+
+const numbers: number[] = [1, 2, 3];
+const messages: string[] = ["Hello", "World"];
+
+messages.push(1);
+
+let mightBeUndefined: string | undefined = undefined;
+let nullableNumber: string | null = null;
+
+let color: "red" | "orange" | "yellow" = "red";
+color = "orange";
+color = "green";
