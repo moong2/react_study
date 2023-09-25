dd48ae0ada5501da0bd5e1089887f3d05ad4d8be
moong2 <www.castlehi@gmail.com>
2023-09-25 18:37:20 +0900

    feat: define type in function

--- dist/practice.js
+++ dist/practice.js
@@ -1,14 +1,5 @@
 "use strict";
-var count = 0;
-count += 1;
-count = "갑자기 분위기 문자열";
-var message = "Hello World";
-var done = true;
-var numbers = [1, 2, 3];
-var messages = ["Hello", "World"];
-messages.push(1);
-var mightBeUndefined = undefined;
-var nullableNumber = null;
-var color = "red";
-color = "orange";
-color = "green";
+function sum(x, y) {
+    return x + y;
+}
+sum(1, 2);
--- src/practice.ts
+++ src/practice.ts
@@ -1,19 +1,5 @@
-let count = 0;
-count += 1;
-count = "갑자기 분위기 문자열";
+function sum(x: number, y: number): number {
+	return x + y;
+}
 
-const message: string = "Hello World";
-
-const done: boolean = true;
-
-const numbers: number[] = [1, 2, 3];
-const messages: string[] = ["Hello", "World"];
-
-messages.push(1);
-
-let mightBeUndefined: string | undefined = undefined;
-let nullableNumber: string | null = null;
-
-let color: "red" | "orange" | "yellow" = "red";
-color = "orange";
-color = "green";
+sum(1, 2);
