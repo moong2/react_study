fbb90afff983a1dd0b18b59913b8088aafd6bd7a
moong2 <www.castlehi@gmail.com>
2023-09-25 20:08:26 +0900

    feat: using generics in class

--- dist/practice.js
+++ dist/practice.js
@@ -1,4 +1,31 @@
 "use strict";
-var items = {
-    list: ["a", "b", "c"]
-};
+var Queue = /** @class */ (function () {
+    function Queue() {
+        this.list = [];
+    }
+    Object.defineProperty(Queue.prototype, "length", {
+        get: function () {
+            return this.list.length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Queue.prototype.enqueue = function (item) {
+        this.list.push(item);
+    };
+    Queue.prototype.dequeue = function () {
+        return this.list.shift();
+    };
+    return Queue;
+}());
+var queue = new Queue();
+queue.enqueue(0);
+queue.enqueue(1);
+queue.enqueue(2);
+queue.enqueue(3);
+queue.enqueue(4);
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
+console.log(queue.dequeue());
