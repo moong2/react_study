a0a5efe79be50bfef54c132bcc587ea4c3e15988
moong2 <www.castlehi@gmail.com>
2023-09-25 20:54:34 +0900

    feat: make new Component with optional props

--- ts-react-tutorial/src/Greeting.tsx
+++ ts-react-tutorial/src/Greeting.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 type GreetingProps = {
 	name: string;
 	mark: string;
+	optional?: string;
 };
 
-function Greetings({name, mark}: GreetingProps) {
+function Greetings({name, mark, optional}: GreetingProps) {
 	return (
 		<div>
 			Hello, {name} {mark}
+			{optional && <p>{optional}</p>}
 		</div>
 	);
 }
